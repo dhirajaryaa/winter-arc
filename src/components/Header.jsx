bc9496043646343a6800c264a69411ac1f14e325
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-11-02 13:02:04 +0000

    style issue fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import { socialLinks } from "@/config/link"
 
 function Header() {
     return (
-        <header className="flex justify-center gap-5 items-center my-8">
-            <nav className="space-x-5 bg-violet-200 rounded-full px-4 py-2">
+        <header className="flex justify-center gap-5 items-center my- px-2">
+            <nav className="flex flex-wrap items-center justify-center gap-5 bg-violet-200 rounded-2xl px-4 py-2">
                 {socialLinks.map((link) => (
                     <a
                         target="_blank"
                         rel="noopener noreferrer"
                         key={link.label}
                         href={link.url}
-                        className="text-lg font-medium capitalize text-violet-800 hover:text-violet-600"
+                        className="text-xs sm:text-lg font-medium capitalize text-violet-800 hover:text-violet-600"
                     >
                         {link.label}↗️
                     </a>
--- src/components/VideoAdd.jsx
+++ src/components/VideoAdd.jsx
@@ -1,13 +1,13 @@
 
 function VideoAdd({ videoLink }) {
     return (
-        <div className="flex justify-center items-center mt-10 w-full">
-            <iframe
-                width="560"
+        <div className="flex justify-center items-center mt-10 w-full px-2">
+            <iframe                width="560"
                 height="315"
                 src={videoLink}
                 title="YouTube video player"
                 frameBorder="0"
+                className="aspect-video h-auto sm:h-66 w-auto"
                 allowFullScreen
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
