8d8feb15fdc89834caca50780b0332b53f93d416
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-11-08 12:57:33 +0000

    day07 video update

--- src/app/day07/page.jsx
+++ src/app/day07/page.jsx
@@ -31,18 +31,17 @@ function day7Page() {
                 </h1>
             </div>
 
-            {/* <VideoAdd videoLink="https://www.youtube.com/embed/j14x5P5nAUw?si=VyShvYboXjEA5WMt" /> */}
-          
+            <VideoAdd videoLink="https://www.youtube.com/embed/RXMJ6G8_zRs?si=hf4pGe6DlUqd8FM1" />
 
             <Section isCompleted={true} title='Image Optimization in Next.js'>
-               <img src="https://images.unsplash.com/photo-1643761440694-afdc3cd06863?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdpbnRlciUyMGFyY3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500" alt="wallpaper"  className="mt-2 w-xs" />
-               <span className="text-background invert-50">unoptimized</span>
-               <p className="mt-2">👉 Next.js Provide <span className="highlighted">{`<Image />`} component use for optimized image rendering</span></p>
-               <Image src="/11.jpg" alt="wallpaper" width={384} height={240}  className="mt-2 "
+                <img src="https://images.unsplash.com/photo-1643761440694-afdc3cd06863?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdpbnRlciUyMGFyY3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500" alt="wallpaper" className="mt-2 w-xs" />
+                <span className="text-background invert-50">unoptimized</span>
+                <p className="mt-2">👉 Next.js Provide <span className="highlighted">{`<Image />`} component use for optimized image rendering</span></p>
+                <Image src="/11.jpg" alt="wallpaper" width={384} height={240} className="mt-2 "
                 // quality={80}
                 // unoptimized
-               />
-               <span className="text-background invert-50">optimized</span>
+                />
+                <span className="text-background invert-50">optimized</span>
 
                 <p className="mt-2">👉 <span className="highlighted">{`<Image />`}</span> component required <span className="highlighted">width and height</span> are important</p>
                 <p className="mt-2">👉 default Image srcset set for different density different image show</p>
@@ -51,8 +50,8 @@ function day7Page() {
                 <p className="mt-2">👉 if default image load not optimized required so set <span className="highlighted">unoptimized</span> value pass for prevent compression</p>
                 <p className="mt-2">👉 default next.js not allow to use external image source.</p>
                 <p className="mt-2">👉 if use external image source so configure on <span className="highlighted">next.config.js</span> add image site hostname</p>
-                 <pre className="p-2 mt-1 bg-gray-200 rounded overflow-auto whitespace-pre-wrap wrap-break-word">
-                        <code className="wrap-break-word whitespace-pre-wrap">{`images : {
+                <pre className="p-2 mt-1 bg-gray-200 rounded overflow-auto whitespace-pre-wrap wrap-break-word">
+                    <code className="wrap-break-word whitespace-pre-wrap">{`images : {
     remotePatterns: [
       {
         hostname: 'images.unsplash.com',
@@ -60,9 +59,9 @@ function day7Page() {
       }
     ]
   }`}</code>
-                    </pre>
-                    <p className="mt-2"> 👉 Image component accept <span className="highlighted">loader() fucntion</span> also provied callback etc and revived props inside<span className="highlighted">src, width and quality</span> important to return src  then set on Image src overwrite etc use for mainly use on image query based optimization accept like cloudinary,imagekit etc site </p>
-                    <p className="mt-2"> 👉 Next.js background use <span className="highlighted">sharp npm library</span> for image optimization.</p>
+                </pre>
+                <p className="mt-2"> 👉 Image component accept <span className="highlighted">loader() fucntion</span> also provied callback etc and revived props inside<span className="highlighted">src, width and quality</span> important to return src  then set on Image src overwrite etc use for mainly use on image query based optimization accept like cloudinary,imagekit etc site </p>
+                <p className="mt-2"> 👉 Next.js background use <span className="highlighted">sharp npm library</span> for image optimization.</p>
 
             </Section>
         </main>
